a705602c0530a603d76012283f8e8e9edff515a1
Hann <162341230+sh4d94@users.noreply.github.com>
2025-02-18 14:23:33 -0600

    Add files via upload

---
+++ TareasHernaniUEA5/Tarea1_Hernani/script.js
@@ -0,0 +1,2 @@
+console.log("aquí está la consola")
+window.alert("Awas, andas de tentón donde no, mi compa")
---
+++ TareasHernaniUEA5/Tarea1_Hernani/variables/script.js
@@ -0,0 +1,8 @@
+//variables
+
+let ajolopeso1 = " gomitas";
+let precio1 = 50;
+
+//document.write(ajolopeso1);
+document.getElementById('demo').innerHTML="Uno de los dulces que vendemos es:" + ajolopeso1;
+document.getElementById('boba').innerHTML="El precio por kilogramo de las " + ajolopeso1 + " es: " + precio1;
