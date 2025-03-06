4f2f10996bde7d00f9e01e85d127715c6562c49b
sh4d94 <junnou4d@gmail.com>
2025-03-05 22:40:52 -0600

    TAREA 2

---
+++ TareasHernaniUEA5/Tarea2_Hernani/script.js
@@ -0,0 +1,9 @@
+let nodo1 = "Las tecnologías web HTML, CSS y JS son clave para cumplir mis metas. Me ayudan a dar vida a ideas rápidamente y hacen que crear contenido sea más accesible y fácil de entender. No obstante, opino que usarlas en exceso o todo el tiempo puede hacerme depender de plantillas y estructuras prehechas, lo que puede llegar a limitar mi capacidad de experimentar y pensar de manera crítica. Creo que estas herramientas son útiles, sí, pero no van a definir hasta dónde puedo llegar creativamente.";
+
+let nodo2 = "Veo mucho potencial en estas tecnologías para mi trabajo, sobre todo para crear cosas visuales en 2D o en 3D, que es lo que más me interesa ahora. Sin embargo, aunque considero que todas estas herramientas digitales ahorran muchísimo tiempo, hacen que todo se vea muy recto, muy igual. Por eso, creo que hay que encontrar un balance entre lo eficiente y lo original, sin dejar que la tecnología le ponga un techo a la imaginación que todos llevamos dentro. Podemos moldear lo preestablecido.";
+
+document.getElementById('nodo1').innerHTML=nodo1;
+document.getElementById('nodo2').innerHTML=nodo2;
+
+console.log(nodo1.length);
+console.log(nodo2.length);
